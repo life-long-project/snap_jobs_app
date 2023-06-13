efe72cf848b408c31f3beb31143e58291a666539
nouran <nouran@gmail.com>
2023-06-14 01:48:21 +0300

    add apiconstant

--- lib/core/utilities_and_theme/app_constants.dart
+++ lib/core/utilities_and_theme/app_constants.dart
@@ -1,3 +1,5 @@
 class AppConstants {
   static const baseUrl = "https://api.themoviedb.org/3";
+  static const profile =
+      'https://jobseeker-profile-api.onrender.com/profile/';
 }
--- lib/profile_feature/data/data source/profile_methods.dart
+++ lib/profile_feature/data/data source/profile_methods.dart
@@ -12,14 +12,22 @@ import '../../../core/network/error_message_model.dart';
 
 abstract class BaseProfileDataSource {
   Future<ProfileModel> getProfile(String id);
+
+
+
+
+  
+
 }
 
 class ProfileDataSource extends BaseProfileDataSource {
   @override
   Future<ProfileModel> getProfile(String id) async {
-//get request using dio
+
     final response = await sl<BaseHttpClient>()
-        .get(Uri.parse('https://jobseeker-profile-api.onrender.com/profile/$id'));
+        .get(Uri.parse(
+          'https://jobseeker-profile-api.onrender.com/profile/$id')
+          );
 
     if (response.statusCode == 200) {
       return (ProfileModel.fromJson(jsonDecode(response.body)['data']));
