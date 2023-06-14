b807b6a17e28de931b0a09128302928243dbbbda
nouran <nouran@gmail.com>
2023-06-14 03:26:35 +0300

    del

--- lib/profile_feature/data/repository/profile_repo.dart
+++ lib/profile_feature/data/repository/profile_repo.dart
@@ -5,20 +5,20 @@ import 'package:snap_jobs/profile_feature/domain/repository/profile_repo.dart';
 //
 import '../../../core/error/exceptions.dart';
 
-import '../data source/profile_methods.dart';
 
-class Profilerepo extends BaseProfilerepo {
-  final BaseProfileDataSource basedatasource;
 
-  Profilerepo(this.basedatasource);
+// class Profilerepo extends BaseProfilerepo {
+//   //final BaseProfileDataSource basedatasource;
 
-  @override
-  Future<Either<Failure, Profile>> getprofile(String id) async {
-    final result = await basedatasource.getProfile(id);
-    try {
-      return right(result);
-    } on ServerException catch (failure) {
-      return left(ServerFailure(failure.errorMessageModel.statusMessage));
-    }
-  }
-}
+//   //Profilerepo(this.basedatasource);
+
+//   @override
+//   Future<Either<Failure, Profile>> getprofile(String id) async {
+//     final result = await basedatasource.getProfile(id);
+//     try {
+//       return right(result);
+//     } on ServerException catch (failure) {
+//       return left(ServerFailure(failure.errorMessageModel.statusMessage));
+//     }
+//   }
+// }
